5f3ec72bd23a99168031d1f1b3d7a98a1ce0c283
alex-main-academy <oleksandrilnytskyi@gmail.com>
2023-07-11 09:58:39 +0300

    fix

--- js/scroll-to-id.js
+++ js/scroll-to-id.js
@@ -1,28 +1,43 @@
 function anchorSetLoadPage() {
-  const anchor = window.location.hash.substr(1).replace('#', '');
-  if(window.location.hash.substr(1) !== '') {
-    const mainSection = document.querySelectorAll('.main > .section');
-    const menuActive = document.querySelectorAll('.menu > .menu__nav > .ul > .menu__item');
+  const anchor = window.location.hash.substr(1).replace("#", "");
+  if (window.location.hash.substr(1) !== "") {
+    const mainSection = document.querySelectorAll(".main > .section");
+    const menuActive = document.querySelectorAll(
+      ".menu > .menu__nav > .ul > .menu__item"
+    );
 
     function removeFirstActiveSection() {
-      mainSection.forEach(function(item) {
-        item.classList.remove('is-active')
-        item.classList.remove('is-visited')
+      mainSection.forEach(function (item) {
+        item.classList.remove("is-active");
+        item.classList.remove("is-visited");
       });
 
-      menuActive.forEach(function(item) {
-        item.classList.remove('is-active')
+      menuActive.forEach(function (item) {
+        item.classList.remove("is-active");
       });
     }
-    if(document.getElementById(anchor) == null) {
+    if (document.getElementById(anchor) == null) {
       return false;
     } else {
       removeFirstActiveSection();
-      document.querySelector('.menu > .menu__nav > .ul > .menu__item[data-id='+ anchor +']').classList.add('is-active');
-      document.getElementById(anchor).classList.add('is-active');
-      document.getElementById(anchor).classList.add('is-visited');
+      document
+        .querySelector(
+          ".menu > .menu__nav > .ul > .menu__item[data-id=" + anchor + "]"
+        )
+        .classList.add("is-active");
+      document.getElementById(anchor).classList.add("is-active");
+      document.getElementById(anchor).classList.add("is-visited");
     }
-  } 
+  }
   return false;
 }
 anchorSetLoadPage();
+
+const anixBox = document.querySelectorAll(".anim-box");
+
+if (window.innerWidth < 768) {
+  anixBox.forEach((elem) => {
+    elem.classList.remove("anim-box");
+    elem.classList.remove("anim-scroll");
+  });
+}
